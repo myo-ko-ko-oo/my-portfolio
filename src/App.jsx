bcb760c8fa8f13343c8a5008ab64b637e96bd13d
myo ko ko oo <myokokooo.5287@gmail.com>
2024-01-12 09:15:54 +0630

    update image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { Avatar} from '@chakra-ui/react'
+import { Avatar} from '@chakra-ui/react';
+import myImage from "../src/assets/image/my-image.jpg"
 function App() {
 
 
@@ -7,7 +8,7 @@ function App() {
     <>
       <div className="text-center text-blue-600 mt-5 font-semibold text-3xl">
         <h1>My Portfoilo</h1>
-        <Avatar name='Dan Abrahmov' size='2xl'className='mt-5' src='https://bit.ly/dan-abramov' />
+        <Avatar name='Dan Abrahmov' size='2xl'className='mt-5' src={myImage} />
       </div>
     </>
   )
